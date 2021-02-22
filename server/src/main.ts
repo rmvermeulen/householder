5faa059c915b422848e7e294fb9a357aad169f73
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-22 11:31:44 +0100

    Server: enable cors for development

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import { Logger } from '@nestjs/common';
+
+const port = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.use(cors());
+  await app.listen(port);
+  console.log(`Server running on ${port}!`);
 }
 bootstrap();
