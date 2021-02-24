b39c43adbc4d598fd4afe3f644401ba3646cfddc
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-24 14:22:40 +0100

    server:e2e: add test for /user

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,30 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(() => app.close());
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/user (GET)', () => {
+    const userMatcher = expect.objectContaining({
+      id: expect.any(Number),
+      firstName: expect.any(String),
+      lastName: expect.any(String),
+      isActive: expect.any(Boolean),
+    });
+    return request(app.getHttpServer())
+      .get('/user')
+      .expect(200)
+      .expect(({ body }) => {
+        expect(body).toBeInstanceOf(Array);
+        for (const item of body) {
+          expect(item).toStrictEqual(userMatcher);
+        }
+      });
   });
 });
