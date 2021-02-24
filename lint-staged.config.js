1a211799317ba0fc52caae517591671e660f77fe
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-24 14:22:30 +0100

    project: seperate lint-staged checks for unit/e2e tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   "client/**/*": "lerna --scope client run test -- ",
-  "server/**/*": "lerna --scope server run test -- ",
+  "server/**/*": [
+    "lerna --scope server run test -- -- --findRelatedTests",
+    "lerna --scope server run test:e2e -- -- --onlyChanged",
+  ],
 };
