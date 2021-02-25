2f6d508801bb3212f0d76af12353a091e6275e94
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-25 23:34:04 +0100

    server:lint-staged: run related e2e tests on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   "client/**/*": "lerna --scope client run test -- ",
   "server/**/*": [
-    // run only tests related to the changed files
     "lerna --scope server run test -- -- --findRelatedTests",
-    (_) =>
-      // ignore files, just run all e2e tests
-      "lerna --scope server run test:e2e",
+    "lerna --scope server run test:e2e -- -- --findRelatedTests",
   ],
 };
